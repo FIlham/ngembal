42f809e3389f58ca254e28b093a4a84e36b15baa
filham <fahmiilham281207@gmail.com>
2026-04-27 17:55:56 +0700

    feat: update layout metadata and add favicon support

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Figtree, Geist, Geist_Mono, Public_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
+import { env } from "@/lib/env";
 
 const publicSansHeading = Public_Sans({
     subsets: ["latin"],
@@ -22,8 +23,27 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-    title: "Ngembal",
+    // title: "Ngembal",
+    metadataBase: new URL(env.BETTER_AUTH_URL), // TODO: Replace with your actual production domain
+    title: {
+        default: "Ngembal",
+        template: "%s | Ngembal", // Automatically adds "| Ngembal" to sub-page titles
+    },
     description: "Sosmed-like web app",
+    icons: {
+        icon: "/favicon.ico",
+    },
+    openGraph: {
+        title: "Ngembal",
+        description: "Sosmed-like web app",
+        type: "website",
+        siteName: "Ngembal",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Ngembal",
+        description: "Sosmed-like web app",
+    },
 };
 
 export default function RootLayout({
